06056908275be6f705b14da2aa2ac2d1677b6eae
Cristian <cchenaor@eafit.edu.co>
2024-10-25 22:43:10 -0500

    PruebaStart2

--- src/test/java/co/edu/eafit/protobootapp/HttpRequestTest.java
+++ src/test/java/co/edu/eafit/protobootapp/HttpRequestTest.java
@@ -5,10 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+//import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.DynamicPropertySource;
+//import org.springframework.boot.web.server.LocalServerPort;
+//import org.springframework.test.context.DynamicPropertySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
