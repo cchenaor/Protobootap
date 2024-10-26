d6a2f45b9865753f068189398ac6a435470e7b68
Cristian <cchenaor@eafit.edu.co>
2024-10-25 19:05:43 -0500

    Pruebas unitarias

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -11,10 +11,5 @@ public class Calculadora {
     public Double cubo(Double numero) {
         return numero * numero * numero;
     }
-    // Método main para pruebas
-    public static void main(String[] args) {
-        Calculadora calc = new Calculadora();
-        System.out.println("Prueba de CI: El cuadrado de 3 es " + calc.cuadrado(3.0));
-        System.out.println("Prueba de CI: El  3 es " + calc.cubo(3.0));
-    }
+    
 }
